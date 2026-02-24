8f652d3ef157c4ecdc18aed1c2c0141477f079e2
sasiiii02 <sasikaru15@gmail.com>
2026-02-24 21:08:47 +0530

    updated headers in route js

--- app/clerk/route.js
+++ app/clerk/route.js
@@ -10,6 +10,7 @@ export async function POST(req){
     const svixHeaders= {
         "svix-id":headerPayload.get("svix-id"),
         "svix-signature":headerPayload.get("svix-signature"),
+        "svix-timestamp":headerPayload.get("svix-timestamp")
     };
 
     //Get the payload and verify it
